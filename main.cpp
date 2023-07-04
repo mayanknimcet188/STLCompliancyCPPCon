f7ea6196a9d3d821cecd2bb998ec87700eaec20b
mayanknimcet188 <me.mayank.0602@gmail.com>
2023-07-04 23:17:58 +0530

    Using input and output iterators instead of containers

--- main.cpp
+++ main.cpp
@@ -1,23 +1,30 @@
 #include <iostream>
 #include <vector>
-template <typename T, typename Predicate>
-std::vector<typename std::vector<T>::iterator> find_all(std::vector<T> &values, Predicate pred)
+#include <iterator>
+
+template <typename InputIterator, typename OutputIterator, typename Predicate>
+OutputIterator find_all(InputIterator first, InputIterator last, OutputIterator output, Predicate pred)
 {
-    std::vector<typename std::vector<T>::iterator> matches;
-    for (auto iter = begin(values); iter != end(values); ++iter)
+    while (first != last)
     {
-        if (pred(*iter))
-            matches.push_back(iter);
+        if (pred(*first))
+        {
+            *output = first;
+            ++output;
+        }
+        ++first;
     }
-    return matches;
+
+    return output;
 }
 
 int main()
 {
     using my_container = std::vector<int>;
     my_container values{3, 4, 5, 4, 5, 6, 5, 8};
-    auto matches = find_all(values, [](int i)
-                            { return i == 5; });
+    std::vector<my_container::iterator> matches;
+    find_all(begin(values), end(values), std::back_inserter(matches), [](int i)
+             { return i == 5; });
 
     std::cout << "Found " << matches.size() << " matches: " << std::endl;
     for (const auto &it : matches)
